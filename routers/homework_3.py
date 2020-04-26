06a851a14e81f639d35c267cf1501d2c05d08cde
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2020-04-26 19:34:56 +0200

    exercise nr 2 from lecture number 3 done

--- routers/homework_3.py
+++ routers/homework_3.py
@@ -1,9 +1,34 @@
-from fastapi import APIRouter
+from hashlib import sha256
+import secrets
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import RedirectResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 router = APIRouter()
+router.secret_key = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+security = HTTPBasic()
 
 
 @router.get("/welcome")
 @router.get("/")
 def welcome_text():
     return {"message": "Welcome to the python world!"}
+
+
+@router.post("/login")
+async def login(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudnY")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    session_token = sha256(str.encode(f"{credentials.username}{credentials.password}{router.secret_key}")).hexdigest()
+    response = RedirectResponse(url='/welcome', status_code=status.HTTP_302_FOUND)
+    response.set_cookie(key="session_token", value=session_token)
+
+    return response
