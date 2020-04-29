7d93bc1ae2ff9be53791411fce6cd009a996ef90
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2020-04-29 10:32:08 +0200

    exercise nr 2 from lecture nr 4 done #1

--- routers/tracks.py
+++ routers/tracks.py
@@ -7,7 +7,6 @@ router = APIRouter()
 @router.on_event("startup")
 async def startup():
     router.db_connection = await aiosqlite.connect('chinook.db')
-    router.db_connection.row_factory = aiosqlite.Row
 
 
 @router.on_event("shutdown")
@@ -17,6 +16,7 @@ async def shutdown():
 
 @router.get("/tracks")
 async def get_tracks(page: int = 0, per_page: int = 10):
+    router.db_connection.row_factory = aiosqlite.Row
     cursor = await router.db_connection.execute(
         "SELECT * FROM tracks ORDER BY TrackId LIMIT :per_page OFFSET :per_page*:page",
         {"page": page, "per_page": per_page})
@@ -26,11 +26,12 @@ async def get_tracks(page: int = 0, per_page: int = 10):
 
 @router.get("/tracks/composers/")
 async def get_composer(response: Response, composer_name: str):
+    router.db_connection.row_factory = lambda cursor, x: x[0]
     cursor = await router.db_connection.execute(
-        "SELECT composer, Name FROM tracks WHERE composer = :composer_name ORDER BY Name",
-        {"composer_name": composer_name})
+        "SELECT Name FROM tracks WHERE composer = ? ORDER BY Name",
+        (composer_name, ))
     tracks_list = await cursor.fetchall()
     if not tracks_list:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"detail": {"error": "No such composer was found"}}
+        return {"detail": {"error": "No such composer was found!"}}
     return tracks_list
