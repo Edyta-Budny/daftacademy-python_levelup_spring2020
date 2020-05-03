38b826a1ab727798d275757773e3c27fcfe75497
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2020-05-03 12:07:00 +0200

    exercise nr 4 from lecture nr 4 done

--- routers/tracks.py
+++ routers/tracks.py
@@ -8,6 +8,16 @@ class Album(BaseModel):
     artist_id: int
 
 
+class Customer(BaseModel):
+    company: str = None
+    address: str = None
+    city: str = None
+    state: str = None
+    country: str = None
+    postalcode: str = None
+    fax: str = None
+
+
 router = APIRouter()
 
 
@@ -34,9 +44,8 @@ async def get_tracks(page: int = 0, per_page: int = 10):
 @router.get("/tracks/composers")
 async def get_composer(response: Response, composer_name: str):
     router.db_connection.row_factory = lambda cursor, x: x[0]
-    cursor = await router.db_connection.execute(
-        "SELECT Name FROM tracks WHERE composer = ? ORDER BY Name",
-        (composer_name, ))
+    cursor = await router.db_connection.execute("SELECT Name FROM tracks WHERE composer = ? ORDER BY Name",
+                                                (composer_name,))
     tracks_list = await cursor.fetchall()
     if not tracks_list:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -46,16 +55,14 @@ async def get_composer(response: Response, composer_name: str):
 
 @router.post("/albums")
 async def add_album(response: Response, album: Album, status_code=201):
-    cursor = await router.db_connection.execute(
-        "SELECT ArtistId FROM artists WHERE ArtistId = ?",
-        (album.artist_id, ))
+    cursor = await router.db_connection.execute("SELECT ArtistId FROM artists WHERE ArtistId = ?",
+                                                (album.artist_id,))
     artist_id = await cursor.fetchall()
     if not artist_id:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"detail": {"error": "No artist found with the given artist_id!"}}
-    cursor = await router.db_connection.execute(
-        "INSERT INTO albums (Title, ArtistId) VALUES (?, ?)",
-        (album.title, album.artist_id))
+    cursor = await router.db_connection.execute("INSERT INTO albums (Title, ArtistId) VALUES (?, ?)",
+                                                (album.title, album.artist_id))
     await router.db_connection.commit()
     response.status_code = status.HTTP_201_CREATED
     return {
@@ -68,8 +75,35 @@ async def add_album(response: Response, album: Album, status_code=201):
 @router.get("/albums/{album_id}")
 async def get_album(album_id: int):
     router.db_connection.row_factory = aiosqlite.Row
-    cursor = await router.db_connection.execute(
-        "SELECT * FROM albums WHERE AlbumId = ?",
-        (album_id, ))
+    cursor = await router.db_connection.execute("SELECT * FROM albums WHERE AlbumId = ?",
+                                                (album_id,))
     album = await cursor.fetchone()
     return album
+
+
+@router.put("/customers/{customer_id}")
+async def update_customer(response: Response, customer_id: int, customer: Customer):
+    cursor = await router.db_connection.execute("SELECT CustomerId FROM customers WHERE CustomerId = ?",
+                                                (customer_id,))
+    customer_id = await cursor.fetchone()
+    if not customer_id:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": {"error": "No customer found with the given customer_id!"}}
+    update_date = customer.dict(exclude_unset=True)
+    if update_date:
+        parameters = list(update_date.values())
+        parameters.append(customer_id)
+        sql = "UPDATE customers SET "
+        for key in update_date.keys():
+            if key == "Postalcode":
+                key = "PostalCode"
+            key.capitalize()
+            sql += f"{key}=?, "
+        sql = sql[:-2] + f" WHERE CustomerId = ?"
+        cursor = await router.db_connection.execute(sql, tuple(parameters))
+        await router.db_connection.commit()
+    router.db_connection.row_factory = aiosqlite.Row
+    cursor = await router.db_connection.execute("SELECT * FROM customers WHERE CustomerId = ?",
+                                                (customer_id, ))
+    customer = await cursor.fetchone()
+    return customer
